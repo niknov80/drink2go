becf2d5c6bc5152545bec22b2e16fedc6d41df87
niknov80 <n.novozhilov80@gmail.com>
2024-01-16 16:56:48 +0300

    Updates

---
+++ js/modules/menu.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector(".user-nav__button"),menu=document.querySelector(".main-nav");export const toggleClickHandler=()=>{toggle.addEventListener("click",(()=>{menu.classList.toggle("main-nav--closed")}))};export const detectedScript=()=>{menu.classList.remove("no-js"),menu.classList.add("main-nav--closed")};
